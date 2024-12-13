b6e19979a2a224849147da4fbb1e6c21f4ed84e2
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2024-12-13 15:43:05 +0300

    feat: solve day 12 part 1

--- src/puzzles/day12.rs
+++ src/puzzles/day12.rs
@@ -1,3 +1,9 @@
+use std::collections::HashMap;
+
+use petgraph::{
+    adj::NodeIndex, algo::condensation, dot::*, prelude::UnGraphMap, visit::IntoNodeReferences,
+};
+
 #[test]
 fn test_part1() {
     let test_string = String::from(
@@ -14,7 +20,7 @@ fn test_part1() {
     );
     let result = solve_part1(&test_string);
 
-    assert_eq!(0, result)
+    assert_eq!(1930, result)
 }
 
 #[test]
@@ -36,34 +42,38 @@ fn test_part2() {
     assert_eq!(0, result)
 }
 
-type GridCell = (char, bool);
-type Grid = Vec<Vec<GridCell>>;
-type Coordinate = (isize, isize);
+type Coordinate = (i32, i32);
+type Map = HashMap<Coordinate, char>;
+type RegionGraph = UnGraphMap<(i32, i32), ()>;
 
-#[derive(Debug, Default)]
-struct Map {
-    grid: Grid,
-    directions: Vec<Coordinate>,
-    height: isize,
-    width: isize,
-}
+const DIRECTIONS: [Coordinate; 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
 
-impl Map {
-    fn contains_coordinate(&self, coordinate: Coordinate) -> bool {
-        let (x, y) = coordinate;
-
-        if x < 0 || y < 0 {
-            return false;
-        }
-
-        self.height > y && self.width > x
-    }
-}
-
-pub fn solve_part1(data: &String) -> i32 {
+pub fn solve_part1(data: &String) -> usize {
+    let mut total = 0;
     let map = read_data(data);
-    println!("{} {}: {:?}", "❗", "map", map);
-    0
+    let region_graph = generate_map_graph(map);
+    let condensed_region_graph =
+        condensation(region_graph.clone().into_graph::<NodeIndex>(), false);
+
+    for (index, node_list) in condensed_region_graph.node_references() {
+        let area = node_list.len();
+        let perimeter = node_list
+            .iter()
+            .map(|node| 4 - region_graph.neighbors(*node).count())
+            .sum::<usize>();
+
+        println!(
+            "{}: {:?} {:?} {:?}",
+            "❗",
+            area,
+            perimeter,
+            area * perimeter
+        );
+
+        total += area * perimeter;
+    }
+
+    total
 }
 
 pub fn solve_part2(data: &String) -> i32 {
@@ -71,21 +81,33 @@ pub fn solve_part2(data: &String) -> i32 {
 }
 
 fn read_data(data: &String) -> Map {
-    let mut map: Map = Map::default();
-    let mut grid: Grid = vec![];
+    data.lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.trim()
+                .chars()
+                .enumerate()
+                .map(move |(x, c)| ((x as i32, y as i32), c))
+        })
+        .collect::<Map>()
+}
 
-    for line in data.lines() {
-        let mut row: Vec<GridCell> = vec![];
-        for letter in line.trim().chars() {
-            row.push((letter, false))
+fn generate_map_graph(map: Map) -> RegionGraph {
+    let mut region_graph: UnGraphMap<(i32, i32), ()> = UnGraphMap::new();
+
+    for ((x, y), c) in map.iter() {
+        let node = region_graph.add_node((*x, *y));
+
+        for direction in DIRECTIONS.iter() {
+            let neighbour_node = (x + direction.0, y + direction.1);
+            if map
+                .get(&neighbour_node)
+                .is_some_and(|neighbour_char| neighbour_char == c)
+            {
+                region_graph.add_edge(node, neighbour_node, ());
+            }
         }
-        grid.push(row);
     }
 
-    map.height = grid.len() as isize;
-    map.width = grid[0].len() as isize;
-    map.directions = Vec::from([(0, 1), (1, 0), (-1, 0), (0, -1)]);
-    map.grid = grid;
-
-    map
+    region_graph
 }
