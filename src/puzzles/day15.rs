966183910bcf7bc573fbf63802d7295a89623c03
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2024-12-19 13:51:02 +0300

    feat: day 15 part 1 is working on test but not real

--- src/puzzles/day15.rs
+++ src/puzzles/day15.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use glam::IVec2;
 use nom::InputIter;
 
@@ -43,101 +45,117 @@ fn test_part2() {
 
 #[derive(Debug, PartialEq, Eq)]
 enum GridObject {
-    Empty,
     Wall,
     Box,
     Robot,
 }
 
-#[derive(Debug, PartialEq, Eq)]
-enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
-}
-
-type Map = Vec<Vec<(GridObject, IVec2)>>;
+type Map = HashMap<IVec2, GridObject>;
 
 pub fn solve_part1(data: &str) -> i32 {
-    let dirs = [
-        IVec2::new(0, -1),
-        IVec2::new(1, 0),
-        IVec2::new(0, 1),
-        IVec2::new(-1, 0),
-    ];
-
     let (mut map, movements) = parse_data(data);
 
     for movement in movements {
-        let robot = find_robot(&map);
-        println!("{} {}: {:?}", "❗", "robot", robot);
+        let robot_coor = map
+            .iter()
+            .find(|(_, object)| object == &&GridObject::Robot)
+            .unwrap()
+            .0
+            .clone();
+
+        let next_pos = robot_coor + movement;
+
+        let Some(next) = map.get(&next_pos) else {
+            map.remove(&robot_coor);
+            map.insert(next_pos, GridObject::Robot);
+
+            continue;
+        };
+
+        match next {
+            GridObject::Wall => continue,
+            GridObject::Robot => {
+                unreachable!("second robot")
+            }
+            GridObject::Box => {
+                let mut movable_items = vec![next_pos];
+
+                while Some(&GridObject::Box)
+                    == map.get(&(movable_items.iter().last().unwrap() + movement))
+                {
+                    movable_items.push(movable_items.iter().last().unwrap() + movement);
+                }
+
+                let Some(_) = map.get(&(movable_items.iter().last().unwrap() + movement)) else {
+                    let robot = map.remove(&robot_coor).unwrap();
+
+                    let next_item_pos = movable_items.iter().next().unwrap();
+                    let next_item = map.remove(next_item_pos).unwrap();
+
+                    map.insert(*next_item_pos, robot);
+
+                    match movable_items.iter().last() {
+                        Some(last_item_pos) => map.insert(*last_item_pos + movement, next_item),
+                        None => map.insert(*next_item_pos + movement, next_item),
+                    };
+
+                    continue;
+                };
+            }
+        }
     }
-    0
+
+    map.iter()
+        .filter(|(_, object)| object == &&GridObject::Box)
+        .map(|(position, _)| 100 * position.y + position.x)
+        .sum()
 }
 
 pub fn solve_part2(data: &str) -> i32 {
     0
 }
 
-fn parse_data(data: &str) -> (Map, Vec<Direction>) {
+fn parse_data(data: &str) -> (Map, Vec<IVec2>) {
     let input_data = data.split("\n\n").collect::<Vec<&str>>();
     let grid_data = input_data[0];
     let movement_data = input_data[1];
 
     let map = parse_grid(grid_data);
-
     let movements = parse_movement(movement_data);
 
     (map, movements)
 }
 
 fn parse_grid(grid_data: &str) -> Map {
-    let mut grid: Map = vec![];
-    for (y, line) in grid_data.lines().enumerate() {
-        let mut row: Vec<(GridObject, IVec2)> = vec![];
+    let mut map: Map = HashMap::new();
 
+    for (y, line) in grid_data.lines().enumerate() {
         for (x, letter) in line.trim().chars().enumerate() {
             let coordinate = IVec2::new(x as i32, y as i32);
 
-            match letter {
-                '#' => row.push((GridObject::Wall, coordinate)),
-                'O' => row.push((GridObject::Box, coordinate)),
-                '@' => row.push((GridObject::Robot, coordinate)),
-                _ => row.push((GridObject::Empty, coordinate)),
+            if letter == '#' {
+                map.insert(coordinate, GridObject::Wall);
+            } else if letter == 'O' {
+                map.insert(coordinate, GridObject::Box);
+            } else if letter == '@' {
+                map.insert(coordinate, GridObject::Robot);
             }
         }
-        grid.push(row);
     }
-    grid
+
+    map
 }
 
-fn parse_movement(movement_data: &str) -> Vec<Direction> {
-    let mut movements: Vec<Direction> = vec![];
+fn parse_movement(movement_data: &str) -> Vec<IVec2> {
+    let mut movements: Vec<IVec2> = vec![];
     for char in movement_data.iter_elements() {
         match char {
-            '^' => movements.push(Direction::Up),
-            '>' => movements.push(Direction::Right),
-            'v' => movements.push(Direction::Down),
-            '<' => movements.push(Direction::Left),
+            '^' => movements.push(IVec2::NEG_Y),
+            '>' => movements.push(IVec2::X),
+            'v' => movements.push(IVec2::Y),
+            '<' => movements.push(IVec2::NEG_X),
             _ => break,
         }
     }
     movements
-}
-
-fn find_robot(map: &Map) -> IVec2 {
-    let mut robot_pos = IVec2::new(0, 0);
-
-    for row in map.iter() {
-        for cell in row.iter() {
-            if cell.0 != GridObject::Robot {
-                continue;
-            }
-
-            robot_pos = cell.1;
-        }
-    }
-
-    robot_pos
 }
